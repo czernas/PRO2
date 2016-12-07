6217b45254b5a381242ff21ea21d2e275bd057b0
czernas <czernas@gmail.com>
2016-12-07 11:55:43 +0100

    .

--- src/obrazek/ManazerObrazku.java
+++ src/obrazek/ManazerObrazku.java
@@ -1,5 +1,16 @@
 package obrazek;
 
-public class ManazerObrazku {
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
 
+public class ManazerObrazku {
+	private Map<String, BufferedImage> mapaObr;
+	private ZdrojObrazku zo;
+	
+	public ManazerObrazku() {
+		mapaObr = new HashMap<>();
+		this.zo = zo;
+		this.zo.naplnMapu();
+	}
 }
